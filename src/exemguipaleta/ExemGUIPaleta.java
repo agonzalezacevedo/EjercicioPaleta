1b55c8b5daeda41167521907e5f678d5a958a237
agonzalezacevedo <56076787+agonzalezacevedo@users.noreply.github.com>
2020-03-31 11:19:47 +0200

    Paleta de copiado y borrado de texto

---
+++ src/exemguipaleta/ExemGUIPaleta.java
@@ -0,0 +1,10 @@
+package exemguipaleta;
+
+public class ExemGUIPaleta {
+
+    public static void main(String[] args) {
+        Tarefa1 ven = new Tarefa1();
+        ven.setVisible(true);
+    }
+    
+}
